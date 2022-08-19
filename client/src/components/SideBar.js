461a53cad54e234141f32c0adae3467361dcf470
Rikuo Hirasawa <rikuoh84@gmail.com>
2022-08-19 20:29:06 -0230

    sidebar setup contd

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,10 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 const Sidebar = () => {
   return (
-    <>
-    </>
+    <Wrapper>
+      <h3>Category</h3>
+    </Wrapper>
   )
 };
 
 export default Sidebar;
+
+const Wrapper = styled.div``
+
+const SubHeading = styled.h3``
