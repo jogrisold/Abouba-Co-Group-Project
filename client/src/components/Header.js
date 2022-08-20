b28418999dd4e3806f0a836d6b8711b1463d22a0
Jo <jogrisold@gmail.com>
2022-08-20 18:21:12 -0400

    Added colors, buttons for cart and profile, start of filters in the sidebar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import styled from "styled-components";
 import { StoreContext } from "./StoreContext";
 import { BsCartDash } from "react-icons/bs";
-import logo from "../assets/logo.png";
+import { FaRegUser } from "react-icons/fa";
+import logoblue from "../assets/logoblue.png";
 
 
 export const Header = () => {
@@ -14,23 +15,29 @@ export const Header = () => {
         setLoggedIn(true);
     };
 
+    const handleClick = (e) => {
+
+    };
+
     return(
         <>
         <Wrapper>
-            <Logo src = {logo}/>
+            <Logo src = {logoblue}/>
+            <FlexRow>
+                <Cart><BsCartDash size = {40}/></Cart>
+                <ProfileBtn handleClick={handleClick}><FaRegUser size = {40}/></ProfileBtn>
+                {
+                loggedIn 
+                ? <LogOut
+                    handleSubmit={(e)=>{handleSubmit(e)}}>
+                    Logout
+                    </LogOut> 
+                : <LogIn
+                    handleSubmit={(e)=> {handleSubmit(e)}}>
+                    Login
+                    </LogIn>}
 
-            <Cart>{BsCartDash}</Cart>
-            {
-            loggedIn 
-            ? <LogOut
-                handleSubmit={(e)=>{handleSubmit(e)}}>
-                Logout
-                </LogOut> 
-            : <LogIn
-                handleSubmit={(e)=> {handleSubmit(e)}}>
-                Login
-                </LogIn>}
-            
+            </FlexRow>
         </Wrapper>
         </>
     )
@@ -40,24 +47,59 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    align-items: center;
+    background-color: var(--color-secondary);
+`;
+const FlexRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 `;
 const Logo = styled.img`
     width: 200px;
 `;
-const Cart = styled.button`
+const Cart = styled.div`
     border-radius: 10px;
+    width: 50px;
+    height: 50px;
     color: white;
-    background-color: var(--color-primary);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--color-secondary);
+    `;
+const ProfileBtn = styled.button`
+    color: white;
+    background-color: var(--color-secondary);
+    border: none;
+    padding-top: 10px;
 `;
 const LogIn = styled.button`
+    font-family: var(--font-heading);
+    font-size: 20px;
+    border: 2px solid white;
     border-radius: 10px;
-    width: 100px;
+    width: 65px;
+    height: 50px;
+    padding: 5px;
     color: white;
-    background-color: var(--color-primary);
+    margin: 10px;
+    align-items: center;
+    text-align: center;
+    background-color: var(--color-secondary);
 `;
 const LogOut = styled.button`
+    font-family: var(--font-heading);
+    font-size: 20px;
+    border: 2px solid white;
     border-radius: 10px;
-    width: 100px;
+    width: 65px;
+    height: 50px;
+    padding: 5px;
     color: white;
-    background-color: var(--color-primary);
+    margin: 10px;
+    align-items: center;
+    text-align: center;
+    background-color: var(--color-secondary);
 `;
