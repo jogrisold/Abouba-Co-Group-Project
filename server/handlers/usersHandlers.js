5490dd415c0dbf48fbaa6ef967cc549b66881b26
Aboubakrine-Gueye <aboubag@hotmail.com>
2022-08-23 13:46:45 -0400

    abouba-handle-user-purchases

--- server/handlers/usersHandlers.js
+++ server/handlers/usersHandlers.js
@@ -9,6 +9,8 @@ const options = {
   useUnifiedTopology: true,
 };
 
+const { v4: uuidv4 } = require("uuid");
+
 // get user by id
 const handleUsersById = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
@@ -38,32 +40,77 @@ const handleUsersById = async (req, res) => {
 };
 
 const handleUserPurchase = async (req, res) => {
-//   const client = new MongoClient(MONGO_URI, options);
-//   const db = client.db("groupProject");
+  const client = new MongoClient(MONGO_URI, options);
+  const db = client.db("groupProject");
+  // Get a new uuid for _id and confirmation of the purchase
+  const genId = uuidv4();
+  // Get today's date for the purchase date
+  const today = new Date();
+  let purchaseTotal = 0;
+  let products = [];
+  let quantities = [];
 
-  //   let newValues = {$push:{"$purchaseHistory"}}
-  //   {$push: {"purchaseHistory"purchaseHistory.$[].products":req.body.products}}
   try {
-    console.log(req.body)
-    // await client.connect();
+    await client.connect();
+    // Aggregate the products price and set it to purchaseTotal
+    // Add the product id and quantity to later update the products collection
+    req.body.products.forEach((product) => {
+      purchaseTotal += Number(product.price.replace("$", ""));
+      products.push({
+        _id: parseInt(product._id),
+        quantity: parseInt(product.quantity),
+      });
+    });
 
-    // let query = { email: req.body.email };
+    let document = {
+      _id: genId,
+      confirmation: genId,
+      purchaseTotal: "$" + purchaseTotal,
+      datePurchased: today,
+      products: [...req.body.products],
+    };
 
-    // let newValues = { $set: { purchaseHistory: req.body.purchase } };
+    let query = { email: req.body.email };
+    let newValues = { $push: { purchaseHistory: document } };
+    let update = null;
 
-    //  const user = await db.collection("users").updateOne(query, newValues);
-    //  loops in the purchase products array and for each productId
-    //  then updates the products collection by updating the numInStock
-    //  using the purchased quantity
+    // Updates the user purchaseHistory array by adding the purchased products
+    update = await db.collection("users").updateOne(query, newValues);
 
-    // query = {};
+    if (update) {
+      //  loops in the purchase products array and for each productId
+      //  then updates the products collection by updating the numInStock
+      //  using the purchased quantity
+      //  Using the data array to build query and newValues to update
 
-    // data = await req.body.purchase.products.map((item) => {
-    //   return { productId: item.productId, NumInStock: item.quantity };
-    // });
-    //  Using the data array to build query and newValues to update
-    //  user = await db.collection("products").updateOne(query, newValues);
-  } catch (err) {}
+      let updateProduct = null;
+
+      await products.forEach(async (element) => {
+        updateProduct = db
+          .collection("products")
+          .updateOne(
+            { _id: element._id },
+            { $inc: { numInStock: -element.quantity } }
+          );
+      });
+
+      if (updateProduct) {
+        res.status(200).json({ status: 200, data: document });
+      } else {
+        res.status(404).json({
+          status: 404,
+          data: document,
+          message: "Invalid product. Not found",
+        });
+      }
+    } else {
+      res
+        .status(404)
+        .json({ status: 404, data: document, message: "Not Found" });
+    }
+  } catch (err) {
+    res.status(500).json({ status: 500, data: req.body, message: err });
+  }
 };
 module.exports = {
   handleUsersById,
