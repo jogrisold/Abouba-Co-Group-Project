124bd7fc2c2bd1ab7eef254b0deaf7cde0e6f84a
Jo <jogrisold@gmail.com>
2022-08-23 01:09:27 -0400

    attempt2

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -70,7 +70,6 @@ const Wrapper = styled.div`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    position: relative;
     background-color: var(--color-secondary);
     box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
 `;
