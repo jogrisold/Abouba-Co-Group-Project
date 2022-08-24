dadccc8b798ff8d69a4adf28cd7d01123a358263
Jo <jogrisold@gmail.com>
2022-08-24 00:36:36 -0400

    jo-productcarddone

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,19 +1,39 @@
+//**************************************************************** */
+// Imports
+//**************************************************************** */
+
+// React dependencies
 import { useContext } from 'react'
-import { StoreContext } from './StoreContext'
 import styled from 'styled-components'
-import {AiOutlineShoppingCart} from 'react-icons/ai'
-import { ImCheckmark } from 'react-icons/im'
-import { FcCancel } from 'react-icons/fc'
 import { useNavigate } from 'react-router-dom'
+
+// Local element dependencies
+import { StoreContext } from './StoreContext'
 import DefaultCircularProgress from './DefaultCircularProgress'
 
+// Icons
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import { FcCancel } from 'react-icons/fc'
 
+// Create an element to render products in Homepage
 export const ProductCard = ({product}) => {
 
+    //**************************************************************** */
+    // Constants
+    //**************************************************************** */
+
+    // Use context to get our products via dispatch reducer, companies 
+    // and cart data
     const { dispatch, companies, cart } = useContext(StoreContext);
+    
+    // Define a constant to use Navigate
     const navigate = useNavigate();
 
-    //Add single product to cart
+    //**************************************************************** */
+    // Functions
+    //**************************************************************** */
+
+    // Create a function to add single product to cart
     const addToCart = (product) => {
         // Check whether the product is already in the cart
         if (cart[product._id]){
@@ -33,10 +53,14 @@ export const ProductCard = ({product}) => {
         dispatch({type: 'add-to-cart', key: product._id, product: product})
     }
 
-    //Navigate to product details page
+    // Function to navigate to product details page associated with that product id
     const handleClickCard = (productId) => {
         navigate(`/product/${productId}`)
     }
+
+    //**************************************************************** */
+    // Render
+    //**************************************************************** */
 
     if (product) {
         //Find the company object to render the company name
@@ -72,15 +96,17 @@ export const ProductCard = ({product}) => {
             </Card>
         )
 
-    // Render loading circle
+    // Otherwise, render loading circle
     } else {
         return <DefaultCircularProgress/>
     }
 }
 
+//**************************************************************** */
+// Styled-Components
+//**************************************************************** */
 
 const Card = styled.div`
-    /* z-index:0; */
     border: 1px solid rgba(120, 120, 120, 0.3);
     width: 300px;
     display: flex;
@@ -95,8 +121,7 @@ const Card = styled.div`
     &:hover {
         transform: scale(1.03);
     }
-`
-
+`;
 const ContentInformation = styled.div`
     display: flex;
     flex-direction: column;
@@ -104,31 +129,31 @@ const ContentInformation = styled.div`
     border-top: 1px solid rgba(120, 120, 120, 0.3);
     padding: 8px 16px;
     gap: 8px;
-`
+`;
 const Flex = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 10px 0;
-`
+`;
 const Image = styled.img`
     width: 200px; 
     max-height: 250px;
     margin: 15px 0;
-`
+`;
 const ProductName = styled.div`
     font-family: var(--font-body);
-`
+`;
 const CompanyName = styled.div`
     width: 100%;
     margin: 5px 0;
     font-family: var(--font-body);
     color: rgba(120, 120, 120, 0.8);
-`
+`;
 const TextBold = styled.p`
     font-family: var(--font-body);
     font-weight: 700;
-`
+`;
 const Cart = styled.button`
     width: 40px;
     height: 40px;
@@ -145,7 +170,7 @@ const Cart = styled.button`
     &:active{
         background-color: lightblue;
     }
-`
+`;
 const OutofStock = styled.button`
     width: 40px;
     height: 40px;
@@ -154,4 +179,4 @@ const OutofStock = styled.button`
     align-items: center;
     border-radius: 50%;
     border: none;
-`
+`;
